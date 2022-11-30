a34c0db4e1bb781bd1240e19a3e40acffb9aeb86
mzhao98 <michelledzhao@gmail.com>
2022-11-30 12:58:49 -0500

    add order as input

--- src/hi_mdp/ring_of_fire_multiprocess.py
+++ src/hi_mdp/ring_of_fire_multiprocess.py
@@ -92,7 +92,7 @@ class RingOfFire():
         # print("self.total_reward", self.total_reward)
         return self.total_reward
 
-def plot_results(experiment_results, num_rounds, savename="images/test.png"):
+def plot_results(experiment_results, num_rounds, true_human_order, savename="images/test.png"):
 
     means = []
     stds = []
@@ -133,20 +133,20 @@ def plot_results(experiment_results, num_rounds, savename="images/test.png"):
     plt.legend()
     plt.xlabel("Round Number")
     plt.ylabel("Collective Reward")
-    plt.title("Teams with First-Order vs Second-Order Robot")
+    plt.title(f"True Human Order d={true_human_order-1}")
     plt.savefig(savename)
     print("saved to: ", savename)
     plt.close()
 
 
-def run_k_rounds(mm_order, seed, num_rounds, vi_type):
+def run_k_rounds(mm_order, seed, num_rounds, vi_type, true_human_order):
     print(f"running seed {seed} ")
     np.random.seed(seed)
 
     robot_agent = Robot_Model((0.9, -0.9, 0.1, 0.3), mm_order=mm_order, vi_type=vi_type)
 
 
-    true_human_agent = True_Human_Model((0.9, 0.1, -0.9, 0.2), 1)
+    true_human_agent = True_Human_Model((0.9, 0.1, -0.9, 0.2), true_human_order)
     rof_game = RingOfFire(robot_agent, true_human_agent)
     rof_game.run_full_game()
 
@@ -160,37 +160,8 @@ def run_k_rounds(mm_order, seed, num_rounds, vi_type):
     # robot_agent.plot_weight_updates(f"exp26_robot_weightupdates_mmorder{mm_order}_{seed}seed.png")
     return collective_scores
 
-def compare_mm():
-    # np.random.seed(0)
-    num_seeds = 100
-    list_of_random_seeds = np.random.randint(0, 100000, num_seeds)
-    # list_of_random_seeds = [76317, 76219, 83657, 54528, 81906, 70048, 89183, 82939, 98333, 52622]
-    # robot_agent = Human_Model((0.9, -0.9, 0.1, 0.3), 2)
 
-    experiment_results = {}
-    num_rounds = 10
-
-    first_results = {x:[] for x in range(num_rounds)}
-    second_results = {x:[] for x in range(num_rounds)}
-
-    with Pool() as pool:
-        first_order_scores = pool.starmap(run_k_rounds, [('first-only', seed_val, num_rounds) for seed_val in list_of_random_seeds])
-        second_order_scores = pool.starmap(run_k_rounds, [('second-only', seed_val, num_rounds) for seed_val in list_of_random_seeds])
-
-        for result in first_order_scores:
-            for round_no in first_results:
-                first_results[round_no].extend(result[round_no])
-
-        for result in second_order_scores:
-            for round_no in first_results:
-                second_results[round_no].extend(result[round_no])
-
-    experiment_results[1] = first_results
-    experiment_results[2] = second_results
-
-    plot_results(experiment_results, num_rounds, f"images/exp25_noisy_true_{num_seeds}-seeds.png")
-
-def run_experiment(vi_type, n_seeds):
+def run_experiment(vi_type, n_seeds, true_human_order):
     # np.random.seed(0)
     num_seeds = n_seeds
     list_of_random_seeds = np.random.randint(0, 100000, num_seeds)
@@ -205,10 +176,10 @@ def run_experiment(vi_type, n_seeds):
     both_results = {x: [] for x in range(num_rounds)}
 
     with Pool(processes=4) as pool:
-        first_order_scores = pool.starmap(run_k_rounds, [('first-only', seed_val, num_rounds, vi_type) for seed_val in list_of_random_seeds])
-        second_order_scores = pool.starmap(run_k_rounds, [('second-only', seed_val, num_rounds, vi_type) for seed_val in list_of_random_seeds])
+        first_order_scores = pool.starmap(run_k_rounds, [('first-only', seed_val, num_rounds, vi_type, true_human_order) for seed_val in list_of_random_seeds])
+        second_order_scores = pool.starmap(run_k_rounds, [('second-only', seed_val, num_rounds, vi_type, true_human_order) for seed_val in list_of_random_seeds])
 
-        both_order_scores = pool.starmap(run_k_rounds, [('both', seed_val, num_rounds, vi_type) for seed_val in list_of_random_seeds])
+        both_order_scores = pool.starmap(run_k_rounds, [('both', seed_val, num_rounds, vi_type, true_human_order) for seed_val in list_of_random_seeds])
 
         for result in first_order_scores:
             for round_no in first_results:
@@ -226,7 +197,7 @@ def run_experiment(vi_type, n_seeds):
     experiment_results[2] = second_results
     experiment_results[3] = both_results
 
-    plot_results(experiment_results, num_rounds, f"images/exp31_opt_fotrue_{vi_type}-actual_100p_{num_seeds}-seeds.png")
+    plot_results(experiment_results, num_rounds, true_human_order, f"images/exp31_opt_true-order-{true_human_order}_{vi_type}-actual_100p_{num_seeds}-seeds.png")
 
     print(f"Model {vi_type}: Results:")
 
@@ -242,9 +213,10 @@ def run_experiment(vi_type, n_seeds):
 
 def run_ablation():
     number_of_seeds = 100
-    run_experiment(vi_type='mmvi', n_seeds = number_of_seeds)
-    run_experiment(vi_type='stdvi', n_seeds = number_of_seeds)
-    run_experiment(vi_type='mmvi-nh', n_seeds= number_of_seeds)
+    true_human_order = 2
+    run_experiment(vi_type='mmvi', n_seeds = number_of_seeds, true_human_order=true_human_order)
+    run_experiment(vi_type='stdvi', n_seeds = number_of_seeds, true_human_order=true_human_order)
+    run_experiment(vi_type='mmvi-nh', n_seeds= number_of_seeds, true_human_order=true_human_order)
 
 if __name__ == "__main__":
     run_ablation()
